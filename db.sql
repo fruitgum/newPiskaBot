a301063e34ea700315b48f58c0491f9a83badc0c
emma <me@fruitgum.space>
2024-03-09 20:08:24 +0400

    sql create script

---
+++ db.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE `newPiska` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */
+
+CREATE TABLE `history` (
+  `chat_id` int DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `message_id` int NOT NULL,
+  `chat_message_id` int DEFAULT NULL,
+  `proceeded` varchar(255) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`user_id`,`message_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+CREATE TABLE `piska` (
+  `user_id` int DEFAULT NULL,
+  `chat_id` bigint DEFAULT NULL,
+  `user_name` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `size` int DEFAULT NULL,
+  `last_run` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci 

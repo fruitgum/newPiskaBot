bf0162d3b0c8a4e7e6f9f8fd13776ecd1859e61e
emma <me@fruitgum.space>
2024-03-12 15:43:22 +0400

    Added zero check for leaderboard

--- src/botCore/leaders.php
+++ src/botCore/leaders.php
@@ -4,9 +4,13 @@ function getLeaders($chat_id, $message_chat_id, $message_id): void
 {
     global $mysqli;
     $leaderBoard='';
-    $getLeaders=$mysqli->query("select username, size from piska where last_run>date_add(now(), interval -1 month) order by size desc");
+    $getLeaders=$mysqli->query("select username, size from piska where last_run>date_add(now(), interval -1 month) and chat_id='".$chat_id."' order by size desc");
     $leadersRows=$getLeaders->num_rows;
     $l=0;
+    if($leadersRows==0){
+        sendMessage("No participants yet", $chat_id, $message_chat_id, $message_id);
+        exit();
+    }
     while($leaders=$getLeaders->fetch_assoc()){
         $username=$leaders['username'];
         $size=$leaders['size'];
